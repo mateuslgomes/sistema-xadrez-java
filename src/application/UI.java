a19a83f5fb6ef7ddb27a7dcb4f1b03be999cf81a
mateuslgomes <mateuslgomes021@gmail.com>
2022-04-26 12:20:44 -0300

    Possible moves of a piece

--- src/application/UI.java
+++ src/application/UI.java
@@ -32,7 +32,7 @@ public class UI {
 	
 	public static void clearScreen() {
 		System.out.print("\033[H\033]2J");
-		System.out.flush();
+		System.out.flush(); 
 	}
 	
 	public static ChessPosition readChessPositon(Scanner sc) {
