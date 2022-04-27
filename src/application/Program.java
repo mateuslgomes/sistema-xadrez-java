96ca5b7ca31b9b41a4ade506d11c33af2b8d846a
mateuslgomes <mateuslgomes021@gmail.com>
2022-04-27 19:39:14 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,7 +38,16 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
-							
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
+						System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
+					chessMatch.replacePromotedPiece(type);
+				}
 			}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
@@ -52,5 +61,4 @@ public class Program {
 		UI.clearScreen();
 		UI.printMatch(chessMatch, captured);
 	}
-
 }
